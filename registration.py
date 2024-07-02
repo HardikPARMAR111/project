c97a9799654d7385c1272770dbc949564fe9b69d
HardikPARMAR111 <155707866+HardikPARMAR111@users.noreply.github.com>
2024-07-02 08:56:01 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
 
 def submit_form():
@@ -18,36 +18,37 @@ def submit_form():
     messagebox.showinfo('Submitted', f'First Name: {first_name}\nLast Name: {last_name}\nAddress: {address}\nEmail: {email}\nPhone Number: {phone_number}')
 
 # Create the main window
-root = tk.Tk()
+root = Tk()
 root.title('Registration Form')
+root.resizable(0,0)
 # Create labels and entry fields
-label_first_name = tk.Label(root, text='First Name')
+label_first_name = Label(root, text='First Name', fg="blue")
 label_first_name.grid(row=0, column=0, padx=10, pady=5)
-entry_first_name = tk.Entry(root)
+entry_first_name = Entry(root)
 entry_first_name.grid(row=0, column=1, padx=10, pady=5)
 
-label_last_name = tk.Label(root, text='Last Name')
+label_last_name = Label(root, text='Last Name', fg="blue")
 label_last_name.grid(row=1, column=0, padx=10, pady=5)
-entry_last_name = tk.Entry(root)
+entry_last_name = Entry(root)
 entry_last_name.grid(row=1, column=1, padx=10, pady=5)
 
-label_address = tk.Label(root, text='Address')
+label_address = Label(root, text='Address', fg="blue")
 label_address.grid(row=2, column=0, padx=10, pady=5)
-entry_address = tk.Entry(root)
+entry_address = Entry(root)
 entry_address.grid(row=2, column=1, padx=10, pady=5)
 
-label_email = tk.Label(root, text='Email')
+label_email = Label(root, text='Email', fg="blue")
 label_email.grid(row=3, column=0, padx=10, pady=5)
-entry_email = tk.Entry(root)
+entry_email = Entry(root)
 entry_email.grid(row=3, column=1, padx=10, pady=5)
 
-label_phone_number = tk.Label(root, text='Phone Number')
+label_phone_number = Label(root, text='Phone Number', fg="blue")
 label_phone_number.grid(row=4, column=0, padx=10, pady=5)
-entry_phone_number = tk.Entry(root)
+entry_phone_number = Entry(root)
 entry_phone_number.grid(row=4, column=1, padx=10, pady=5)
 
 # Submit button
-submit_button = tk.Button(root, text='Submit', command=submit_form)
+submit_button = Button(root, text='Submit', command=submit_form, width=30, fg="white", bg="blue")
 submit_button.grid(row=5, column=0, columnspan=2, pady=10)
 
 
