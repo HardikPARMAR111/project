fe63c604ddb052ea674c60d3a32119d61d25fa69
HardikPARMAR111 <155707866+HardikPARMAR111@users.noreply.github.com>
2024-07-06 08:51:44 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
 user_credentials = {
     'user1': 'password1',
@@ -14,27 +14,30 @@ def check_credentials():
         messagebox.showinfo("Login Successful", f"Welcome, {username}!")
     else:
         messagebox.showerror("Login Failed", "Invalid username or password")
-        entry_password.delete(0, tk.END)  # Clear the password entry field
+        entry_password.delete(0, END)  # Clear the password entry field
 
 # Create main window
-root = tk.Tk()
+root = Tk()
 root.title("Login Page")
+root.attributes('-toolwindow',True)
+root.resizable(0,0)
+
 
 # Create username label and entry
-label_username = tk.Label(root, text="Username:")
-label_username.pack(pady=10)
-entry_username = tk.Entry(root)
-entry_username.pack(pady=5)
+label_username = Label(root, text="Username:")
+label_username.grid(row=0,column=0,padx=20,pady=10)
+entry_username = Entry(root)
+entry_username.grid(row=0,column=1,padx=15,pady=10)
 
 # Create password label and entry
-label_password = tk.Label(root, text="Password:")
-label_password.pack(pady=10)
-entry_password = tk.Entry(root, show='*')
-entry_password.pack(pady=5)
+label_password = Label(root, text="Password:")
+label_password.grid(row=1,column=0,padx=20,pady=20)
+entry_password = Entry(root, show='*')
+entry_password.grid(row=1,column=1,padx=15,pady=20)
 
 # Create login button
-btn_login = tk.Button(root, text="Login", width=10, command=check_credentials)
-btn_login.pack(pady=20)
+btn_login = Button(root, text="Login", width=10, command=check_credentials)
+btn_login.grid(rowspan=1,columnspan=2)
 
 # Start the GUI application
 root.mainloop()
